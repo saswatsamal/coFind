1aac85a6bc796700a1046c447b23b4c3705d4c31
saswatsamal <samalsaswat0@gmail.com>
2021-09-30 23:28:44 +0530

    [add] : Details, [Update] : Styling

---
+++ js/data.js
@@ -0,0 +1,11 @@
+const userData = [
+    {
+        userName: "Saswat Samal",
+        userImage: "https://avatars.githubusercontent.com/u/39031660?v=4",
+        userDesignation: "Web Developer",
+        userBio:"A student developer who loves to help people by developing products.",
+        linkedinID: "saswatsamal",
+        twitterID: "TweetSaswat",
+    },
+    
+];
--- js/script.js
+++ js/script.js
@@ -35,3 +35,89 @@ var square = document.createElement("div");
                     }
                 );
             });
+
+
+//-----------------------------------------------------------------------------------//
+
+//Typing Effect
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
+//-----------------------------------------------------------------------------------//
+
+
+//User Data Display
+function userCard(user) {
+  return `
+  <div class="profile-card">
+  <div class="profile-image"><img src="${user.userImage}" alt="${user.userName}" width=200/></div>
+  <figcaption>
+    <h3>${user.userName}</h3>
+    <h5>${user.userDesignation}</h5>
+    <p>${user.userBio}</p>
+    <div class="icons">
+      <a href="https://twitter.com/${user.twitterID}" targert="blank"> <img src="https://img.icons8.com/color/50/000000/twitter-circled.png"/></i></i></a>
+      <a href="https://linkedin.com/in/${user.linkedinID}"> <img src="https://img.icons8.com/color/48/000000/linkedin-circled.png"/></i></a>
+    </div>
+  </figcaption>
+  </div>
+`;
+}
+
+document.getElementById("app").innerHTML = `
+${userData.map(userCard).join("")}
+`;
