5d19007de6d4f90b27119a605867559d7cdcfb36
Ankit Raj Biswal <ankitrudra2001@gmail.com>
2021-10-02 08:05:36 +0530

    Updated User Details

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
 const userData = [
     {
-        userName: "Saswat Samal",
-        userImage: "https://avatars.githubusercontent.com/u/39031660?v=4",
+        userName: "Ankit Raj Biswal",
+        userImage: "https://user-images.githubusercontent.com/53295960/135701139-16e94dda-2514-4681-9daa-4346c653bb21.jpg",
         userDesignation: "Web Developer",
-        userBio:"A student developer who loves to help people by developing products.",
-        linkedinID: "saswatsamal",
-        twitterID: "TweetSaswat",
+        userBio:"They told that i can find the source code of 🌏 here, Still didn't find it let me know if you do ",
+        linkedinID: "ankit-raj-biswal-9705051a4",
+        twitterID: "AnkitRajBiswal4",
     },
     
 ];
