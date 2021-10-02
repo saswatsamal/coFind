e5b01ac776630bef37ec9197c16228e673b00a5f
Saswat Mohanty <mohantysaswat154@gmail.com>
2021-10-02 23:23:20 +0530

    Added details to data.js

--- js/data.js
+++ js/data.js
@@ -22,5 +22,13 @@ const userData = [
         userBio:"A student developer who always opens to learn and explore new technologies. Loves hackathons.",
         linkedinID: "arpan-aditya",
         twitterID: "adityaa_sahoo",
+    },
+    {
+        userName: "Saswat Mohanty",
+        userImage: "https://avatars.githubusercontent.com/u/53933493?s=400&u=a468cb7093fa0ebd5a2118d290c8d24bff8bd035&v=4",
+        userDesignation: "Web Developer",
+        userBio:"A student developer, eager to learn new stuffs, meet new people. Big time food lover",
+        linkedinID: "saswat-mohanty-3460321b4",
+        twitterID: "Saswat7101",
     }
 ];
